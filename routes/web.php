897176d3f12ac97df1d3b95f3eb5cc37d18367bb
Arc-Judecca <gilangfibarkah125@gmail.com>
2023-11-22 14:17:14 +0700

    Ini untuk Jetstream Auth nya

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
+   
 use Illuminate\Support\Facades\Route;
-
+   
+use App\Http\Livewire\Products;
+  
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+   
+Route::get('products', Products::class)->middleware('auth');
